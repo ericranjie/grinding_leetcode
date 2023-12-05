61f9f9801e1c20e220b8f8eb9cfc12c3e49af56c
ericran <ericranjie1991@gmail.com>
2023-12-05 20:28:18 +0800

    Add solutions.

--- 1-100/94_BinaryTreeInorderTraversal.cc
+++ 1-100/94_BinaryTreeInorderTraversal.cc
@@ -11,21 +11,44 @@
  */
 class Solution {
 public:
-    /** Recursive Solution:
-      */
+/** Recursive Solustion:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-    
-        Inorder(res, root);
-
+        Inorder(root, res);
         return res;
     }
 
-    void Inorder(vector<int> &res, TreeNode* root) {
+    void Inorder(TreeNode *root, vector<int> &res) {
         if (!root) return;
-
-        Inorder(res, root->left);
+        Inorder(root->left, res);
         res.push_back(root->val);
-        Inorder(res, root->right);
+        Inorder(root->right, res);
+    }
+*/
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode *> s;
+        vector<int> res;
+        if (!root) return res;
+        TreeNode *cur = root;
+
+        s.push(cur);
+        // cur = cur->left; // EPoint
+        while (!s.empty()) {
+            while (cur->left) { // EPoint
+                cur = cur->left;
+                s.push(cur);
+            }
+            while (!s.empty()) {
+                cur = s.top(); s.pop();
+                res.push_back(cur->val);
+                if (cur->right) {
+                    cur = cur->right; // EPoint
+                    s.push(cur);
+                    break;
+                }
+            }
+        }
+        
+        return res;
     }
 };
